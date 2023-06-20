57b8001ba5042b02a2732891945c3e170a4110c7
lamtdang <lamtdang98@gmail.com>
2023-06-20 12:38:01 +0700

    change task type

--- celeryq/task.py
+++ celeryq/task.py
@@ -4,5 +4,5 @@ from ml import train_model, predict
 
 
 @shared_task(ignore_result=False)
-def train_ml(data_source: str) -> MLModel:
+def train_ml(data_source: str) -> bytes:
     return train_model(data_source=data_source)
